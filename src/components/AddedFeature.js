4f7b5c93e70cef881c2dd58e88bc9a8931f0707f
Brandon Desselle <desselle.brandon2@gmail.com>
2019-08-14 17:08:11 -0500

    Update AddedFeature.js

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -4,8 +4,12 @@ const AddedFeature = props => {
   return (
     <React.Fragment>
       <li>
-        {/* Add an onClick to run a function to remove a feature */}
-        <button className="button">X</button>
+        <button
+          onClick={() => props.removeFeature(props.feature)}
+          className="button"
+        >
+          X
+        </button>
         {props.feature.name}
       </li>
     </React.Fragment>
