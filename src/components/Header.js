0e8b0b29ce0a7d97904b7ee086487da82a3e6765
Brandon Desselle <desselle.brandon2@gmail.com>
2019-08-14 17:08:21 -0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,27 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const Header = props => {
-  return (
-    <React.Fragment>
-      <figure className="image is-128x128">
-        <img src={props.car.image} alt={props.car.name} />
-      </figure>
-      <h2>{props.car.name}</h2>
-      <p>Amount: ${props.car.price}</p>
-    </React.Fragment>
-  );
+class Header extends React.Component {
+  render() {
+    return (
+      <>
+        <figure className="image is-128x128">
+          <img src={this.props.car.image} alt={this.props.car.name} />
+        </figure>
+        <h2>{this.props.car.name}</h2>
+        <p>Amount: ${this.props.car.price}</p>
+      </>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    car: state.car
+  };
 };
 
-export default Header;
+export default connect(
+  mapStateToProps,
+  {}
+)(Header);
