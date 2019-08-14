89226c1d3f4d9682aef28e2434d117f5d9e47103
Brandon Desselle <desselle.brandon2@gmail.com>
2019-08-14 17:08:16 -0500

    Update AdditionalFeature.js

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -2,13 +2,12 @@ import React from "react";
 
 const AdditionalFeature = props => {
   return (
-    <React.Fragment>
-      <li>
-        {/* Add an onClick that will let you add a feature to your car */}
-        <button className="button">Add</button>
-        {props.feature.name} (+{props.feature.price})
-      </li>
-    </React.Fragment>
+    <li>
+      <button onClick={() => props.buyItem(props.feature)} className="button">
+        Add
+      </button>
+      {props.feature.name} (+{props.feature.price})
+    </li>
   );
 };
 
